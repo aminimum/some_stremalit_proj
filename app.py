29b48dbbc2c79e4d942cc3eae874ad4bd0f52dd9
Амин Буранкулов <johngolt19@gmail.com>
2025-02-07 00:39:06 +0500

    init commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.title("Visualization")
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+
+st.pyplot(fig)
+
+
+# option = st.sidebar.selectbox(
+#     "Choose partition",
+#     ("Home", "Analyze", "Settings")
+# )
+
+# if option == "Home":
+#     st.title("Home")
+# elif option == "Analyze":
+#     st.title("Analyze")
+# else:
+#     st.title("Settings")
