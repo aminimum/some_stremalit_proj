841d38816b6b6718bea9acf6fe2dc2891533e659
aminimum <72760330+aminimum@users.noreply.github.com>
2025-02-07 00:46:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,25 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.title("Visualization")
+# st.title("Visualization")
 
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+# x = np.linspace(0, 10, 100)
+# y = np.sin(x)
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
+# fig, ax = plt.subplots()
+# ax.plot(x, y)
 
-st.pyplot(fig)
+# st.pyplot(fig)
 
 
-# option = st.sidebar.selectbox(
-#     "Choose partition",
-#     ("Home", "Analyze", "Settings")
-# )
+option = st.sidebar.selectbox(
+    "Choose partition",
+    ("Home", "Analyze", "Settings")
+)
 
-# if option == "Home":
-#     st.title("Home")
-# elif option == "Analyze":
-#     st.title("Analyze")
-# else:
-#     st.title("Settings")
+if option == "Home":
+    st.title("Home")
+elif option == "Analyze":
+    st.title("Analyze")
+else:
+    st.title("Settings")
